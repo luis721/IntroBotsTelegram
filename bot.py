84117b235f0303407eb421742f7fe8691ef3e2c2
Luis Manuel Cárcamo Morales <luismanuelcarcamo@gmail.com>
2020-05-16 18:36:24 -0500

    Envío de audios e imágenes. Ademaś de inline queries

--- bot.py
+++ bot.py
@@ -1,3 +1,8 @@
+from telegram import InlineQueryResultArticle, InputTextMessageContent, \
+ InlineQueryResultVideo, ParseMode
+from uuid import uuid4
+import traceback
+
 # TODA función que responde a comandos en el bot
 # lleva los parámetros update y context
 
@@ -11,3 +16,69 @@ def prueba(update, context):
     mensaje = "Bienvenido, {}.".format(nombre)
     update.message.reply_text(mensaje)
 
+
+def prueba2(update, context):
+	try:
+		texto = update.message.text
+		mensaje = "El usuario escribió {}".format(texto)
+		update.message.reply_text(mensaje)
+	except Exception as e:
+		print(e)
+		#traceback.print_stack()
+		traceback.print_tb() # mostrar traceback
+		#traceback.print_exception()
+
+
+def imagen(update, context):
+	# Obtener la ID del chat
+	chat_id = update.message.chat.id
+	# archivo
+	img = open('./multimedia/imagen.jpg', 'rb')
+	# Enviar imagen
+	context.bot.send_photo(chat_id, photo=img)
+
+
+def sonido(update, context):
+	chat_id = update.message.chat.id
+	# archivo
+	snd = open("./multimedia/audio.mp3", 'rb')
+	context.bot.send_audio(chat_id, audio=snd)
+
+
+def inline_query(update, context):
+	mensaje = "Este es un mensaje de prueba"
+	# opciones a desplegar
+	results = [
+		InlineQueryResultArticle(
+			id=uuid4(), # SE REQUIERE
+			title="Prueba",
+			input_message_content=InputTextMessageContent(mensaje),
+			description="Test."
+		),
+		InlineQueryResultArticle(
+			id=uuid4(), # SE REQUIERE
+			title="Prueba2",
+			input_message_content=InputTextMessageContent(mensaje),
+			description="Test2."
+		),
+		# pending
+		InlineQueryResultVideo(
+			id=uuid4(),
+			title="Recibe un video",
+			video_url="https://www.youtube.com/watch?v=2HDuqHv3zos",
+			mime_type="video/mp4",
+			thumb_url="https://2.bp.blogspot.com/-LfB9P5GRyIY/VjETrBoHwHI/AAAAAAAAH4Q/5naYJfDbPqM/s1600/google_buscador.png"
+		)
+	]
+
+	# enviar resultado
+	try:
+		update.inline_query.answer(results, cache_time=2)
+	except Exception as e:
+		traceback.print_stack()
+		print(e)
+
+
+# Función para el manejo de errores
+def error(update, context):
+    logger.warning('La solicitud "%s" causó el error "%s"', update, context.error)
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from config import TOKEN
-from telegram.ext import Updater, CommandHandler
-from bot import start, prueba
+from telegram.ext import Updater, CommandHandler, InlineQueryHandler
+#from telegram import 
+import bot
 # @mi_primer_bot_ist_bot
 
 
@@ -8,11 +9,24 @@ def main():
     updater = Updater(TOKEN, use_context=True)
     dispatcher = updater.dispatcher
 
+    # COMANDOS
+
     # Crea un comando llamado start
     # que es manejado por la función start
-    dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(CommandHandler("prueba", prueba))
-
+    dispatcher.add_handler(CommandHandler("start", bot.start))
+    dispatcher.add_handler(CommandHandler("prueba", bot.prueba))
+    dispatcher.add_handler(CommandHandler("prueba2", bot.prueba2))
+    dispatcher.add_handler(CommandHandler("imagen", bot.imagen))
+    dispatcher.add_handler(CommandHandler("sonido", bot.sonido))
+    # inline queries 
+    """
+    Para poder permitir las inline queries en el bot
+    es necesario activarlas primero en Bot Father.
+    """
+    dispatcher.add_handler(InlineQueryHandler(bot.inline_query))
+    # Para el manejo de errores
+    dispatcher.add_error_handler(bot.error)
+    # INICIAR EL BOT
     updater.start_polling()
     updater.idle()
 
